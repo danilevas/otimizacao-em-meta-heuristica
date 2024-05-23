ce59961c5382a0627a7fc2fdb4671372b5858b55
danilevas <dlevacov@gmail.com>
2024-05-23 14:56:17 -0300

    Update pso.py

--- 3pso/pso.py
+++ 3pso/pso.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Define the Rastrigin function
+# Define a função de Rastrigin
 def rastrigin(x):
     n = len(x)
     return 10*n + sum([xi**2 - 10*np.cos(2*np.pi*xi) for xi in x])
@@ -13,9 +13,9 @@ def rosenbrock(X, a=1, b=100):
     return (a - x)**2 + b * (y - x**2)**2
 
 # Define o algoritmo de PSO
-def pso(funcao, dim=2, num_particulas=30, max_iter=100, w=0.5, c1=1, c2=2):
+def pso(funcao, dim=2, limites=[-5.12, 5.12], num_particulas=30, max_iter=100, w=0.5, c1=1, c2=2):
     # Inicializa partículas e velocidades
-    particulas = np.random.uniform(-5.12, 5.12, (num_particulas, dim))
+    particulas = np.random.uniform(limites[0], limites[1], (num_particulas, dim))
     velocidades = np.zeros((num_particulas, dim))
 
     # Inicializa as melhores posições e os melhores valores de fitness
@@ -23,6 +23,8 @@ def pso(funcao, dim=2, num_particulas=30, max_iter=100, w=0.5, c1=1, c2=2):
     melhor_fitness = np.array([funcao(p) for p in particulas])
     swarm_melhor_posicao = melhores_posicoes[np.argmin(melhor_fitness)]
     swarm_melhor_fitness = np.min(melhor_fitness)
+
+    plota(funcao, limites, swarm_melhor_posicao, swarm_melhor_fitness)
 
     # Itera o número especificado de vezes, atualizando a velocidade e posição de cada partícula a cada iteração
     for i in range(max_iter):
@@ -44,9 +46,34 @@ def pso(funcao, dim=2, num_particulas=30, max_iter=100, w=0.5, c1=1, c2=2):
         if np.min(valores_fitness) < swarm_melhor_fitness:
             swarm_melhor_posicao = particulas[np.argmin(valores_fitness)]
             swarm_melhor_fitness = np.min(valores_fitness)
+        
+        if (i+1) % 25 == 0:
+            plota(funcao, limites, swarm_melhor_posicao, swarm_melhor_fitness)
 
     # Retorna a melhor solução encontrada pelo algoritmo de PSO
     return swarm_melhor_posicao, swarm_melhor_fitness
+
+def plota(funcao, limites, solucao, fitness):
+    # Cria uma meshgrid para visualização
+    x = np.linspace(-5.12, 5.12, 100)
+    y = np.linspace(-5.12, 5.12, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = rosenbrock([X, Y])
+
+    # Cria um plot 3D da função
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    # Plota a solução encontrada pelo algoritmo de PSO
+    ax.scatter(solucao[0], solucao[1], fitness, color='red')
+    plt.show()
+
+def roda_30(funcao, dim=2, num_particulas=30, max_iter=100, w=0.5, c1=1, c2=2):
+    pass
 
 # Define as dimensões do problema
 dim = 2
@@ -57,21 +84,3 @@ solucao, fitness = pso(rosenbrock, dim=dim)
 # Printa a solução e valor de fitness
 print('Solução:', solucao)
 print('Fitness:', fitness)
-
-# Cria uma meshgrid para visualização
-x = np.linspace(-5.12, 5.12, 100)
-y = np.linspace(-5.12, 5.12, 100)
-X, Y = np.meshgrid(x, y)
-Z = rosenbrock([X, Y])
-
-# Cria um plot 3D da função
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z, cmap='viridis')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-# Plota a solução encontrada pelo algoritmo de PSO
-ax.scatter(solucao[0], solucao[1], fitness, color='red')
-plt.show()
